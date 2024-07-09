dbd688e5e54aebfe7949ac9a236e65ed4f2619ff
deepak076 <deepak076@ncuindia.edu>
2024-07-09 06:46:27 +0530

    Cart System

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,4 @@
+// controllers\shop.js
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
@@ -44,8 +45,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
